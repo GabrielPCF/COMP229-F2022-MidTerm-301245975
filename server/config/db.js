d57baeba0d41aec650468b1053c7fb2a57c43d94
GabrielPCF <gabrielfulber@gmail.com>
2022-10-25 22:12:29 -0400

    Added all buttons Edit button  GET method works Delete button works Add button goes to right page, still needs logic to work

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://GabrielF:Gabi1209@booksdatabase.tpbnknv.mongodb.net/?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://GabrielF:Gabi1209@booksdatabase.tpbnknv.mongodb.net/books?retryWrites=true&w=majority"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -29,7 +29,8 @@ router.get('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-
+    
+    res.render('books/details', {title: 'Add Book', books: ''});
 });
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -38,7 +39,8 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-
+    
+    
 });
 
 // GET the Book Details page in order to edit an existing Book
@@ -47,6 +49,18 @@ router.get('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    var id = req.params['id'];
+    book.findById(id, (err, books) => {
+      if (err) {
+        return console.error(err);
+      }
+      else {
+        res.render('books/details', {
+          title: 'Edit Book',
+          books: books
+        });
+      }
+    });
 });
 
 // POST - process the information passed from the details form and update the document
@@ -64,6 +78,16 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    var id = req.params['id'];
+    book.findByIdAndRemove(id, (err, books) => {
+      if (err) {
+        return console.error(err);
+      }
+      else {
+        res.redirect('/books');
+      }
+    });
+    
 });
 
 
